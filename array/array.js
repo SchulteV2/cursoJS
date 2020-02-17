c731451c8adb32f8cf499c2daaa19c4e3896b5ed
Vinicius Schulte <schultev2@gmail.com>
2020-02-17 00:06:00 -0300

    revisao de melhorias e adições com o esnext

--- array/array.js
+++ array/array.js
@@ -3,7 +3,7 @@ console.log(typeof Array, typeof new Array, typeof [])
 let aprovados = new Array('Bia', 'Carlos', 'Ana')
 console.log(aprovados)
 
-aprovaods = ['Bia', 'Carlos', 'Ana']
+aprovados = ['Bia', 'Carlos', 'Ana']
 console.log(aprovados[0])
 console.log(aprovados[1])
 console.log(aprovados[2])
